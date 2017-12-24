0d958c12fe8cf8a5d4709bd853652f3d06bde798
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 21:59:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
     db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)", [name, type])
   end
 
-  # def self.save(name, breed, age, database_connection)
-  #   database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
-  # end
+  def .find
+
+  end
 end
