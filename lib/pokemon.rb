4eab1d5640493860e23fb47b0748253386cdfa98
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 22:46:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
 
   def self.find(poke_id, db)
     found = db.execute("SELECT * FROM Pokemon WHERE Pokemon.id = ?",[poke_id])
-    binding.pry
+    # binding.pry
     new = Pokemon.new(found.split(/[\s,]+/), db)
     new
   end
