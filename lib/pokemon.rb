8529ddc51d0a119f7ba659fd065643b836ee63a8
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 20:33:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+
+  def initialize(name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @all << self
+  end
+
+  def save
+
+  end
 end
