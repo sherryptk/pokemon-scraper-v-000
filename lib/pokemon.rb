361600329ce19b1b8707184e96226f2377c44510
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 22:20:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,7 @@ class Pokemon
   end
 
   def self.find(poke_id, db)
-    Pokemon.new(db.execute("SELECT * FROM Pokemon WHERE Pokemon.id = ?",[poke_id]), db)
+    Pokemon.new(db.execute("SELECT * FROM Pokemon WHERE Pokemon.id = ?",[poke_id]), db).save
+
   end
 end
